d12c1e64057c033d543641f0eb6a777d630c1588
Lucas Franzolin <lucasfranzolin@gmail.com>
2018-07-18 16:09:42 -0300

    stock listing and remove functions added to the my_portfolio

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
 
   get 'search_stocks', to: 'stocks#search'
 
-  resources :user_stocks, only: [:create]
+  resources :user_stocks, only: [:create, :destroy]
 
 end
